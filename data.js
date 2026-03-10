edcf504a6de7d5568d78b253009377f7ffa2c37a
artifactstudioua <artifactstudio.ua@gmail.com>
2026-03-10 11:49:23 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,65 @@
+const exhibitsData = [
+  {
+    "id": "exhibit_1", 
+    "name": "Теребовлянська фігурка",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item1.glb",
+    "audioUrl": "./assets/audio/item1.mp3",
+    "imageUrl": "./assets/images/item1.jpg",
+    "mindIndex": 0 // порядковый номер в файле targets.mind
+  },
+  {
+    "id": "exhibit_2",
+    "name": "Зуб Мамонта",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item2.glb",
+    "audioUrl": "./assets/audio/item2.mp3",
+    "imageUrl": "./assets/images/item2.jpg",
+    "mindIndex": 1
+  },
+  {
+    "id": "exhibit_3",
+    "name": "Козацька люлька",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item3.glb",
+    "audioUrl": "./assets/audio/item3.mp3",
+    "imageUrl": "./assets/images/item3.jpg",
+    "mindIndex": 2
+  },
+  {
+    "id": "exhibit_4",
+    "name": "Трипільский горщик",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item4.glb",
+    "audioUrl": "./assets/audio/item4.mp3",
+    "imageUrl": "./assets/images/item4.jpg",
+    "mindIndex": 3
+  },
+  {
+    "id": "exhibit_5",
+    "name": "Великодня тріскачка",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item5.glb",
+    "audioUrl": "./assets/audio/item5.mp3",
+    "imageUrl": "./assets/images/item5.jpg",
+    "mindIndex": 4
+  },
+  {
+    "id": "exhibit_6",
+    "name": "Дереворит Остапа Пасіки",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item6.glb",
+    "audioUrl": "./assets/audio/item6.mp3",
+    "imageUrl": "./assets/images/item6.jpg",
+    "mindIndex": 5
+  },
+  {
+    "id": "exhibit_7",
+    "name": "Кам'яна булава",
+    "description": "Опис",
+    "modelUrl": "./assets/models/item7.glb",
+    "audioUrl": "./assets/audio/item7.mp3",
+    "imageUrl": "./assets/images/item7.jpg",
+    "mindIndex": 6
+  }
+];
